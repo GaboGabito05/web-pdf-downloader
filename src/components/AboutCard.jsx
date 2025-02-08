581106924956ab02f36732f1208729dc52007290
Gabicho <pcjhonny13@gmail,com>
2025-02-08 16:34:14 -0500

    Empezamos a usar componentes React

--- src/components/AboutCard.jsx
+++ src/components/AboutCard.jsx
@@ -1,20 +1,32 @@
+import PropTypes from 'prop-types';
 import '../styles/About.css';
 
 function AboutCard({ icon, title, description, link, button }) {
     return (
         <div className="about-card">
             <div className="card-icon">
-                {icon}
+                <img src={icon} alt="icon" />
             </div>
             <h3>{title}</h3>
             <p>{description}</p>
             <a
                 href={link}
                 className="social-link"
-                target="_blank">{button}</a
+                target="_blank"
+                rel="noopener noreferrer"
             >
+                {button}
+            </a>
         </div>
     )
 }
 
-export default AboutCard
+AboutCard.propTypes = {
+    icon: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    link: PropTypes.string.isRequired,
+    button: PropTypes.string.isRequired,
+};
+
+export default AboutCard;
